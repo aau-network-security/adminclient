337f0b86b756b4d68ba51148f90187826d333573
Mikkel Høst Christiansen <mikkel.christiansen250996@gmail.com>
2023-09-22 22:48:45 +0200

    Team modal done

--- src/components/events/EventTeamsTable.js
+++ src/components/events/EventTeamsTable.js
@@ -1,6 +1,15 @@
 import {
+    Accordion,
+    AccordionButton,
+    AccordionIcon,
+    AccordionItem,
+    AccordionPanel,
+    Box,
     Button,
+    Center,
     Flex,
+    HStack,
+    Heading,
     IconButton,
     Input,
     InputGroup,
@@ -23,20 +32,24 @@ import {
     Th,
     Thead,
     Tr,
+    VStack,
     useDisclosure,
+    useToast,
 } from "@chakra-ui/react";
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { FaPlay, FaStop } from "react-icons/fa";
 import { IoMdAdd } from "react-icons/io";
 import { useDispatch, useSelector } from "react-redux";
 import { Tooltip } from "react-tooltip";
-import { fetchEventTeams } from "../../features/teams/teamSlice";
+import { fetchEventTeams, forceTeamSolve, setSolvedStatus } from "../../features/teams/teamSlice";
 import { MdDelete, MdRefresh } from "react-icons/md";
 import moment from "moment";
 import { SearchIcon } from "@chakra-ui/icons";
+import { cloneDeep, debounce } from "lodash";
 
 function EventTeamsTable() {
-    const teams = useSelector((state) => state.team.teams);
+    const teamsState = useSelector((state) => state.team.teams);
+    const [teams, setTeams] = useState(teamsState)
     const selectedEvent = useSelector((state) => state.event.selectedEvent);
     const dispatch = useDispatch();
     useEffect(() => {
@@ -60,22 +73,204 @@ function EventTeamsTable() {
         setClickedTeam(key);
         onTeamModalOpen();
     };
-    const TeamModal = () => {
+
+    const toast = useToast()
+    const toastIdRef = React.useRef()
+    const forceSolve = async (exerciseTag) => {
+        let request = {
+            eventTag: selectedEvent.tag,
+            exerciseTag: exerciseTag,
+            teamName: teams[clickedTeam].username,
+          }
+          try {
+            const response = await dispatch(forceTeamSolve(request)).unwrap()
+            toastIdRef.current = toast({
+              title: 'Challenge successfully solved',
+              description: "The attempt to force solve a challenge was successful",
+              status: 'success',
+              duration: 5000,
+              isClosable: true,
+            })
+            console.log(teams[clickedTeam])
+            let exercises = teams[clickedTeam].labInfo.exercises
+            console.log(exercises)
+            exercises.forEach((exercise, parentIdx) => {
+                console.log("parent: ", parentIdx, exercise)
+                let childExercises = exercise.childExercises
+                console.log(childExercises)
+                childExercises.forEach((childExercise, childIdx) => {
+                    if (childExercise.tag === exerciseTag) {
+                        dispatch(setSolvedStatus({
+                            team: clickedTeam,
+                            parentIdx: parentIdx,
+                            childIdx: childIdx,
+                            solveStatus: true
+                        }))
+                        return
+                    }
+                })
+            })
+          } catch (err) {
+            console.log(err)
+            toastIdRef.current = toast({
+              title: 'Error solving challenge',
+              description: err.apiError.status,
+              status: 'error',
+              duration: 5000,
+              isClosable: true,
+            })
+          }
+    }
+    
+    const TeamModal = ({teams}) => {
         let username = "";
         if (teams.length) {
             username = teams[clickedTeam].username;
+        }
+        if (!teams[clickedTeam]) {
+            return (
+                <></>
+            )
         }
         return (
             <Modal
                 onClose={onTeamModalClose}
                 isOpen={isTeamModalOpen}
-                isCentered
+                isCentered        
             >
                 <ModalOverlay />
-                <ModalContent>
+                <ModalContent height="600px" width="1300px" maxWidth={"1300px"} display="flex">
                     <ModalHeader>{username}</ModalHeader>
                     <ModalCloseButton />
-                    <ModalBody></ModalBody>
+                    <ModalBody height="450px" display="flex">
+                    {teams[clickedTeam].labInfo.tag === "" ? (
+                                    <Center height="100%" width="100%">
+                                        Team currently has no lab
+                                    </Center>
+                                ) : (
+                        <HStack spacing={10} height="100%" width="100%">
+                            <Flex 
+                                height="100%" 
+                                width="100%" 
+                                bg="aau.bg" 
+                                borderRadius="5px"
+                                padding="0px 10px 0 10px"
+                                flexDir="column"
+                            >
+                                <Text className="container-header-text">
+                                    Lab Containers
+                                </Text>
+                                
+                                <Accordion allowMultiple overflowY="auto" height={"100%"}>
+                                        {Object.entries(teams[clickedTeam].labInfo.exercises).map(([key, exercise]) => (
+                                            <>
+                                            {exercise.machines.length > 0 ? (
+                                                <AccordionItem key={key}>
+                                                    <h2>
+                                                    <AccordionButton>
+                                                        <Box as="span" flex='1' textAlign='left'>
+                                                        <b>{exercise.tag}</b>
+                                                        </Box>
+                                                        <AccordionIcon />
+                                                    </AccordionButton>
+                                                    </h2>
+                                                    <AccordionPanel pb={4}>
+                                                        <TableContainer>
+                                                            <Table size="sm">
+                                                                <Thead position="sticky" zIndex="100">
+                                                                    <Tr>
+                                                                        <Th className='custom-table-header'>Id</Th>
+                                                                        <Th className='custom-table-header'>Type</Th>
+                                                                        <Th className='custom-table-header'>Registry url</Th>
+                                                                        <Th className='custom-table-header'>Status</Th>
+                                                                    </Tr>
+                                                                </Thead>
+                                                                <Tbody>
+                                                                    {Object.entries(exercise.machines).map(([key, machine]) => (
+                                                                        <Tr key={key}>
+                                                                            <Td>{machine.id}</Td>
+                                                                            <Td>{machine.type}</Td>
+                                                                            <Td style={{textWrap: "wrap"}}>{machine.image}</Td>
+                                                                            <Td
+                                                                                color={machine.status === "running" ? "aau.green" : "aau.red"}
+                                                                            >
+                                                                                {machine.status}
+                                                                            </Td>
+                                                                        </Tr>
+                                                                    ))}
+                                                                </Tbody>
+                                                            </Table>
+                                                        </TableContainer>
+                                                    
+                                                    </AccordionPanel>
+                                                </AccordionItem>
+                                            ):(
+                                                <AccordionItem key={key}>
+                                                    <h2>
+                                                    <AccordionButton>
+                                                        <Box as="span" flex='1' textAlign='left'>
+                                                        <b>{exercise.tag}</b>
+                                                        </Box>
+                                                        <AccordionIcon />
+                                                    </AccordionButton>
+                                                    </h2>
+                                                    <AccordionPanel pb={4}>
+                                                        Exercise has not yet been started                                                    
+                                                    </AccordionPanel>
+                                                </AccordionItem>
+                                            )}
+                                            </>
+                                            
+                                        ))}
+                                </Accordion>
+                            </Flex>
+                            <Flex 
+                                height="100%" 
+                                width="100%"
+                                bg="aau.bg" 
+                                borderRadius="5px"
+                                
+                                flexDir="column"
+                            >
+                                <Text className="container-header-text" padding="0px 10px 0 10px"> 
+                                    Challenge flags
+                                </Text>
+                                <VStack align='stretch' height="100%" overflowY="auto" padding="0px 10px 0 10px">
+                                {Object.entries(teams[clickedTeam].labInfo.exercises).map(([key, exercise]) => (
+                                    <React.Fragment key={key}>
+                                        {Object.entries(exercise.childExercises).map(([key, childExercise]) => (
+                                                <Flex key={key}>
+                                                    <Flex flexDir="column">
+                                                        <Heading fontSize="15px">
+                                                            {childExercise.name}
+                                                        </Heading>
+                                                        <Text>{childExercise.flag}</Text>
+                                                    </Flex>
+                                                    <Spacer/>
+                                                    <Button 
+                                                        colorScheme="aau.button" 
+                                                        isDisabled={childExercise.solved}
+                                                        onClick={() => forceSolve(childExercise.tag)}
+                                                    >
+                                                        {childExercise.solved ? (
+                                                            <>
+                                                                Solved
+                                                            </>
+                                                        ):(
+                                                            <>
+                                                                Solve
+                                                            </>
+                                                        )}
+                                                    </Button>
+                                                </Flex>              
+                                        ))}
+                                    </React.Fragment>
+                                ))}
+                                </VStack>
+                            </Flex>
+                        </HStack>
+                        )}
+                    </ModalBody>
                     <ModalFooter>
                         <Button onClick={onTeamModalClose}>Close</Button>
                     </ModalFooter>
@@ -83,6 +278,31 @@ function EventTeamsTable() {
             </Modal>
         );
     };
+
+    const [searchValue, setSearchValue] = useState("");
+    
+    const changeSearchData = (text, teams) => {
+        if (text === "") {
+          setTeams(cloneDeep(teams));
+        } else {
+            setTeams(
+                cloneDeep(
+                teams.filter((el) => {
+                    return el.username.toLowerCase().indexOf(text.toLowerCase()) > -1;
+                })
+                )
+            )
+        }
+      };
+      const debounceLoadData = useCallback(debounce(changeSearchData, 500), []);
+
+      useEffect(() => {
+        debounceLoadData(searchValue, teamsState);
+      }, [searchValue, teamsState]);
+
+      useEffect(() => {
+        setTeams(teamsState)
+      }, [teamsState])
     return (
         <>
             <Flex className="container-header" height="60px">
@@ -93,14 +313,18 @@ function EventTeamsTable() {
                     Teams
                 </h2>
                 <Spacer />
+                {selectedEvent && (
                 <InputGroup width="300px" margin="auto">
                     <InputLeftElement pointerEvents="none">
                         <SearchIcon color="aau.primary" />
                     </InputLeftElement>
-                    <Input type="tel" placeholder="Search teams" />
+                    <Input type="tel" placeholder="Search teams" value={searchValue} onChange={(e) => setSearchValue(e.target.value)}/>
                 </InputGroup>
+                )}
             </Flex>
-            <TableContainer overflowY="unset" h="88%">
+            {selectedEvent && (
+                <>
+                <TableContainer overflowY="unset" h="88%">
                 <Table variant="simple" size="sm">
                     <Thead
                         position="sticky"
@@ -109,9 +333,13 @@ function EventTeamsTable() {
                         backgroundColor="white"
                     >
                         <Tr>
-                            <Th textAlign="center">
-                                Reset lab core/Delete user
-                            </Th>
+                            {
+                                selectedEvent && selectedEvent.status === "running" && (
+                                    <Th textAlign="center">
+                                        Reset lab core/Delete user
+                                    </Th>
+                                )
+                            }
                             <Th>Teamname</Th>
                             <Th>Email</Th>
                             <Th>Status</Th>
@@ -122,6 +350,8 @@ function EventTeamsTable() {
                     <Tbody>
                         {Object.entries(teams).map(([key, team]) => (
                             <Tr key={key}>
+                                {
+                                selectedEvent && selectedEvent.status === "running" && (
                                 <Td width="200px" textAlign="center">
                                     <IconButton
                                         color="aau.primary"
@@ -146,15 +376,25 @@ function EventTeamsTable() {
                                         data-tooltip-id="tooltip-delete-user"
                                     />
                                 </Td>
+                                )}
                                 <Td>
-                                    <Link
-                                        textDecoration="underline"
-                                        _hover={{ color: "aau.primary" }}
-                                        fontWeight={600}
-                                        onClick={() => openTeamModal(key)}
-                                    >
-                                        {team.username}
-                                    </Link>
+                                    {
+                                    selectedEvent && selectedEvent.status === "running" ? (
+                                        <Link
+                                            textDecoration="underline"
+                                            _hover={{ color: "aau.primary" }}
+                                            fontWeight={600}
+                                            onClick={() => openTeamModal(key)}
+                                        >
+                                            {team.username}
+                                        </Link>
+                                    ) : (
+                                        <Text>
+                                            {team.username}
+                                        </Text>
+                                    )
+                                }
+                                    
                                 </Td>
                                 <Td>
                                     {/*Make link */}
@@ -178,9 +418,12 @@ function EventTeamsTable() {
                     </Tbody>
                 </Table>
             </TableContainer>
-            <TeamModal />
+            <TeamModal teams={teams}/>
             <Tooltip id={"tooltip-delete-user"} style={{ zIndex: "9999" }} />
             <Tooltip id={"tooltip-reset-lab"} style={{ zIndex: "9999" }} />
+            </>
+            )}
+            
         </>
     );
 }
--- src/features/teams/teamSlice.js
+++ src/features/teams/teamSlice.js
@@ -5,6 +5,7 @@ const initialState = {
     status: "",
     teams: [],
     error: "",
+    challengesSolving: {},
     statusCode: "",
 }
 export const fetchEventTeams = createAsyncThunk('team/fetchEventTeams', async (req, {rejectWithValue}) => {
@@ -26,10 +27,33 @@ export const fetchEventTeams = createAsyncThunk('team/fetchEventTeams', async (r
     }
 })
 
+export const forceTeamSolve = createAsyncThunk('team/forceTeamSolve', async (challenge, {rejectWithValue}) => {
+    try {
+        apiClient.defaults.headers.Authorization = localStorage.getItem('token')
+        const response = await apiClient.post('teams/solve', challenge)
+        
+        return response.data
+    }
+    catch (err) {
+        if (!err.response) {
+            throw err
+        }
+        let error = { axiosMessage: err.message, axiosCode: err.code, apiError: err.response.data, apiStatusCode: err.response.status}
+        return rejectWithValue(error)
+    }
+})
+
 const teamSlice = createSlice({
     name: 'team',
     initialState,
+    reducers: {
+        setSolvedStatus: (state, action) => {
+            console.log(action.payload)
+            state.teams[action.payload.team].labInfo.exercises[action.payload.parentIdx].childExercises[action.payload.childIdx].solved = action.payload
+        }
+    },
     extraReducers: (builder) => {
+        // FetchEventTeams
         builder.addCase(fetchEventTeams.pending, (state) => {
             state.status = "fetching"
         })
@@ -41,10 +65,27 @@ const teamSlice = createSlice({
         builder.addCase(fetchEventTeams.rejected, (state, action) => {
             state.status = "idle"
             state.teams = []
-            state.error = action.payload.data.status
-            state.statusCode = action.payload.status
+            state.error = action.payload.apiError.status
+            state.statusCode = action.payload.apiStatusCode
+        })
+        // ForceTeamSolve
+        builder.addCase(forceTeamSolve.pending, (state, action) => {
+            state.status = "solving"
+            state.challengesSolving[action.meta.arg.exerciseTag] = 'solving'
+        })
+        builder.addCase(forceTeamSolve.fulfilled, (state, action) => {
+            state.status = "idle"
+            state.error = ''
+            delete state.challengesSolving[action.meta.arg.exerciseTag]
+        })
+        builder.addCase(forceTeamSolve.rejected, (state, action) => {
+            state.status = "idle"
+            state.error = action.payload.apiError.status
+            state.statusCode = action.payload.apiStatusCode
+            delete state.challengesSolving[action.meta.arg.exerciseTag]
         })
     }
 })
 
 export default teamSlice.reducer
+export const { setSolvedStatus } = teamSlice.actions
