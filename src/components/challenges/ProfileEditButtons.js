5eef8ab96fd57b957035bf7d4ca50c5ed3d1fd14
KristianHLarsen <kristianhkl@gmail.com>
2023-10-02 18:15:34 +0200

    added delete button for profiles and a mockup for editing profiles

--- src/components/challenges/ProfileEditButtons.js
+++ src/components/challenges/ProfileEditButtons.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { deleteProfile, fetchProfiles } from '../../features/profiles/profileSlice'
 import { useDispatch, useSelector } from 'react-redux';
-import { IconButton } from '@chakra-ui/react';
+import { Button, Flex, IconButton } from '@chakra-ui/react';
 import { MdDelete } from 'react-icons/md';
 import ProfileDialogDelete from './ProfileDialogDelete';
 function ProfileEditButtons() {  
@@ -34,14 +34,30 @@ function ProfileEditButtons() {
   return (
     <>
     <div>ProfileEditButtons</div>
-        <IconButton
-            aria-label='Delete Profile'
-            colorScheme='aau.buttonRed'
-            variant='ghost'
-            fontSize="20px"
-            icon={<MdDelete />}
-            onClick={() => openAlertDialog(selectedProfile.name)}                  
-            /> 
+    <Flex
+        width={"100%"}
+        marginTop="20px"
+        justifyContent={"center"}
+    >
+            <Button
+                
+                colorScheme="aau.buttonRed"
+                color="white"
+                onClick={() => openAlertDialog(selectedProfile.name)}         
+                marginRight="30px"         
+            >
+                Delete Profile
+            </Button>
+        
+            <Button
+                colorScheme="aau.button"
+                color="white"
+                type="submit"
+                // onClick={() => openAlertDialog(selectedProfile.name)}                  
+            >
+                Edit Profile
+            </Button>
+    </Flex>
              <ProfileDialogDelete 
                     profileName={profileNameState}
                     isOpen={isAlertOpen}
