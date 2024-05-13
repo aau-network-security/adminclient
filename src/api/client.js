e570658fb3b21942ecbdaf6095d488b968509e13
Kristian Larsen <kristianhkl@gmail.com>
2024-05-13 11:04:36 +0200

    added documentation

--- src/api/client.js
+++ src/api/client.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
-
+// set API endpoint for the daemon read
 export const BASE_URL = "http://beta.haaukins.com:8080/v1/admin/"
 
 export const URL_txt = "beta.haaukins.com" // Used for ux when url for events are shown etc.
 
 const apiClient = axios.create({
-  // Later read this URL from an environment variable
+  // set API endpoint for the daemon 
   baseURL: "http://beta.haaukins.com:8080/v1/admin"
 });
 
